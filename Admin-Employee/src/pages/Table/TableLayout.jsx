84129fd4cc523d543e3464ca17995eec0ae6e8c2
giabao6262 <22520111@gm.uit.edu.vn>
2025-01-01 23:21:28 +0700

    Edit TableService UI

--- Admin-Employee/src/pages/Table/TableLayout.jsx
+++ Admin-Employee/src/pages/Table/TableLayout.jsx
@@ -1,59 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import DishModal from './DishModal';
-import './Table.css'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import DishModal from "./DishModal";
+import "./Table.css";
+import axios from "axios";
 
 function TableLayout() {
-    const url = "http://localhost:3056";
-    const [tables,setTables] = useState([]);
-    const [selectedTable, setSelectedTable] = useState(null);
-    const [selectedTableName, setSelectedTableName] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const url = "http://localhost:3056";
+  const [tables, setTables] = useState([]);
+  const [selectedTable, setSelectedTable] = useState(null);
+  const [selectedTableName, setSelectedTableName] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        async function fetchTables() {
-            try {
-                
-                const response = await axios.get(`${url}/api/table/list`);
-                setTables(response.data.data);
+  useEffect(() => {
+    async function fetchTables() {
+      try {
+        const response = await axios.get(`${url}/api/table/list`);
+        setTables(response.data.data);
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
 
-            } catch (error) {
-                throw new Error(error);
-            }
-        }
+    fetchTables();
+  }, []);
 
-        fetchTables();
-    },[])
+  const handleTableClick = (tableId, tableName) => {
+    setSelectedTable(tableId);
+    setSelectedTableName(tableName);
+    setIsModalOpen(true);
+  };
 
-    const handleTableClick = (tableId,tableName) => {
-        setSelectedTable(tableId);
-        setSelectedTableName(tableName)
-        setIsModalOpen(true);
-    };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
-    return (
-        <div className='table-container'>
-            {tables.map((table) => (
-                <div key={table._id}>
-                    <button className='table-btn' onClick={() => handleTableClick(table._id,table.name)}>
-                        {table.name}
-                    </button>
-                </div>
-            ))}
-
-            {isModalOpen && (
-                <DishModal
-                    tableId={selectedTable}
-                    tableName={selectedTableName}
-                    onClose={closeModal}
-                />
-            )}
+  return (
+    <div className="table-container">
+      {tables.map((table) => (
+        <div key={table._id}>
+          <button
+            className="table-btn"
+            onClick={() => handleTableClick(table._id, table.name)}
+          >
+            {table.name}
+            <br />
+            {table.capacity} seats
+          </button>
         </div>
-    )
+      ))}
+
+      {isModalOpen && (
+        <DishModal
+          tableId={selectedTable}
+          tableName={selectedTableName}
+          onClose={closeModal}
+        />
+      )}
+    </div>
+  );
 }
 
-export default TableLayout
+export default TableLayout;
